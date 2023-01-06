ed2ec463a3efc9a21348e1a64c85930f0720f8b3
Joost van Driel <joostvdriel@gmail.com>
2023-01-06 15:51:58 +0100

    Added linter and formatter

--- apps/sswt-api/index.ts
+++ apps/sswt-api/index.ts
@@ -1,16 +1,15 @@
-import express, {Express, Request, Response} from "express"
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
-const dotenv = require('dotenv')
+dotenv.config();
 
-dotenv.config()
+const app: Express = express();
+const port = process.env.PORT || 3000;
 
-const app: Express = express()
-const port = process.env.PORT || 3000
-
-app.get('/', (req, res) => {
-    res.json({msg: 'Hello World'})
-})
+app.get("/", (req: Request, res: Response) => {
+  res.json({ msg: "Hello World, Soliddev!" });
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
