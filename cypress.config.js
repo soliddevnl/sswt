8531f0800f4ca11a637388f00737ec030decf291
Joost van Driel <joostvdriel@gmail.com>
2023-02-21 06:13:46 +0100

    Added acceptance stage

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
+    baseUrl: process.env.WEB_TRACKER_HOST + ":" + process.env.WEB_TRACKER_PORT,
     specPattern: "tests/acceptance/*.spec.ts",
   },
 });
