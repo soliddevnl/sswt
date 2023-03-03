273453c9cff4b37683f397506f870f842486e002
Joost van Driel <joostvdriel@gmail.com>
2023-03-03 05:54:25 +0100

    Replaced nextjs with plain react app

--- apps/sswt-frontend/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  output: "standalone",
-  publicRuntimeConfig: {
-    apiUrl: process.env.API_URL,
-  },
-};
---
+++ apps/sswt-frontend/src/App.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState("Hello!");
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/v1/hello?name=UAT Tester`, {
+      headers: {
+        Authorization: `Bruh`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>{message}</p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
